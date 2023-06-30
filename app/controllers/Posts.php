7e6c839e14e92699acaebecf32e7146a8dbcfebc
Cody Koscielski <codykoscielski@gmail.com>
2023-06-29 22:43:53 -0400

    Started on posts functionality

---
+++ app/controllers/Posts.php
@@ -0,0 +1,19 @@
+<?php
+    #[AllowDynamicProperties] class Posts extends Controller{
+        public function __construct(){
+            if(!isLoggedIn()) {
+                redirect('users/login');
+            }
+
+            $this->postModel = $this->model('Post');
+        }
+
+        public function index() {
+            //Get the posts
+            $posts = $this->postModel->getPosts();
+            $data = [
+                'posts' => $posts
+            ];
+            $this->view('posts/index', $data);
+        }
+    }

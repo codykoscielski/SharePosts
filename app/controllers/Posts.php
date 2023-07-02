1eef0b9670ae2b4ca1327439f1ba887a4b38ad22
Cody Koscielski <codykoscielski@gmail.com>
2023-07-02 08:51:59 -0400

    Added the ability to add a post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -18,11 +18,45 @@
         }
 
         public function add() {
-            $posts = $this->postModel->getPosts();
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
-            $this->view('posts/add', $data);
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //Sanitize POST array
+                $_POST = filter_input_array(htmlspecialchars(INPUT_POST));
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_error' => '',
+                    'body_error' => ''
+                ];
+
+                //Validate the title
+                if(empty($data['title'])) {
+                    $data['title_error'] = 'Please enter a title';
+                }
+                //Validate the Body
+                if(empty($data['body'])) {
+                    $data['body_error'] = 'Please write a post';
+                }
+
+                //Make sure there are no errors
+                if(empty($data['title_error']) && empty($data['body_error'])) {
+                     //Validated
+                    if($this->postModel->addPost($data)) {
+                        flash('post_message', 'Post added!');
+                        redirect('posts');
+                    } else {
+                       die('Something went wrong');
+                    }
+                } else {
+                    //Load the view with errors
+                    $this->view('posts/add', $data);
+                }
+            } else {
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+                $this->view('posts/add', $data);
+            }
         }
     }
--- app/models/Post.php
+++ app/models/Post.php
@@ -21,4 +21,19 @@
             $results = $this->db->resultSet();
             return $results;
         }
+
+        public function addPost($data) {
+            //Insert statement
+            $this->db->query('INSERT INTO posts (title, user_id, body) VALUES (:title, :user_id, :body)');
+            //Bind the values
+            $this->db->bind(':title', $data['title']);
+            $this->db->bind(':user_id', $data['user_id']);
+            $this->db->bind(':body', $data['body']);
+
+            if($this->db->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
